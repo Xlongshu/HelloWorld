e5e48e3583ede047b0029c08ea65adbd01a7f787
Xlongshu <349759857@qq.com>
2016-08-27 00:05:22 +0800

    add line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld{
+	
 	public static void main(String[] args){
 		hello("longshu");
 	}
+	
 	public static void hello(String name){
 		System.out.println("Hello "+name);
 	}
+	
 }
